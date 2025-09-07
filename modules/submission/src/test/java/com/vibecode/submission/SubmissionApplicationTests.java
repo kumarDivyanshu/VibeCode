488333795e705adb2f48cc5c1afc1acd60d6d311
Divyanshu Kumar <divyanshukumar258@gmail.com>
2025-09-07 21:08:30 +0530

    submission module creation

---
+++ modules/submission/src/test/java/com/vibecode/submission/SubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibecode.submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
